a7e6f694952ec444b5cd12858fab78c20f5b688a
Amin <sahidhussain012@gmail.com>
2020-08-29 13:50:28 +0530

    This is modified

--- src/main/java/Academy/App.java
+++ src/main/java/Academy/App.java
@@ -1,13 +1,16 @@
 package Academy;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hi!" );
+        System.out.println( "Amin" );
+        System.out.println( "Hussain!" );
+        System.out.println( "Sahid!" ); 
+        System.out.println( "Waris!" );
+
     }
 }
