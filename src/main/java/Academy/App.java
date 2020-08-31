db94df76587a5f23320f427e30b28b956723ec5d
Amin <sahidhussain012@gmail.com>
2020-08-31 12:40:17 +0530

    Default Code

--- src/main/java/Academy/App.java
+++ src/main/java/Academy/App.java
@@ -1,27 +1,11 @@
 package Academy;
 
-import org.testng.annotations.Test;
+
 
 public class App 
 {
-   @Test
-   public void Automation()
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("Hi");
-    }
-   @Test
-   public void Appium()
-    {
-        System.out.println( "Mobile testing!" );
-        System.out.println("cell phone software testing");
-    }
-   @Test
-   public void RestAPI(){
-	   System.out.println("API is running");
-   }
-   @Test
-   public void Selenium(){
-	   System.out.println("Selenium is up and running");
-   }
+	public static void main(String[] args){
+		
+		System.out.println("hello world!");
+	}
 }
